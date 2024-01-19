6a7f810945b345c04eba37379dc6737dca1e07ca
tedy97123 <tedyyohanes97@gmail.com>
2024-01-19 15:19:08 -0800

    update to main.tex

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 import "@/index.css";
-import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "@/state/api";
@@ -14,7 +13,7 @@ export const store = configureStore({
 setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Provider store={store}>
+
     <App />
-  </Provider>
+ 
 );
